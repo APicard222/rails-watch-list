485c89f814d19ffe5093d47207c17101e1c79ce1
Alexandre Picard <alex.picard@bluewin.ch>
2021-11-18 15:50:07 +0100

    Fully functionnal Model

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,28 @@
+class BookmarksController < ApplicationController
+
+  def new
+    @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    @bookmark.save
+    # no need for app/views/bookmarks/create.html.erb
+    redirect_to list_path(@list)
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list)
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :list_id, :movie_id)
+  end
+end
